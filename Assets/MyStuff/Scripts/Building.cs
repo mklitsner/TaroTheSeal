95ed4904798b132e0b8cbd9afc59b6e0ea044e45
Miller Klitsner <mklitsner@gmail.com>
2023-09-30 12:01:53 -0700

    added components for building and damage

---
+++ Assets/MyStuff/Scripts/Building.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Building : MonoBehaviour, IDamageable
+{
+    [SerializeField] int starterHealth=10;
+    [SerializeField] int currHealth;
+
+
+    private void Start()
+    {
+        currHealth = starterHealth;
+    }
+
+
+    public void DoDamage(int amount)
+    {
+        currHealth -= amount;
+
+        if (currHealth < 0) currHealth = 0;
+
+        if (currHealth == 0)
+        {
+            Console.WriteLine("building is destroyed");
+            Destroy(this);
+        }
+        else
+        {
+            Console.WriteLine($"building damaged, {currHealth} health remaining");
+        }
+    }
+}
+
